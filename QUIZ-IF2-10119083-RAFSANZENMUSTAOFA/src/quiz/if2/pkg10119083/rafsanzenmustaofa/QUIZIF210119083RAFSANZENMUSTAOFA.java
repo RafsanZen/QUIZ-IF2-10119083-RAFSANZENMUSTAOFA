53d95ba30b65b51979f322fd663f7f0b9d761a25
Rafsan Zen M <73044188+RafsanZen@users.noreply.github.com>
2020-11-27 21:26:35 +0700

    Add files via upload

---
+++ QUIZ-IF2-10119083-RAFSANZENMUSTAOFA/src/quiz/if2/pkg10119083/rafsanzenmustaofa/QUIZIF210119083RAFSANZENMUSTAOFA.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz.if2.pkg10119083.rafsanzenmustaofa;
+
+/**
+ *
+ * Rafsan Zen M
+ * 10119083
+ */
+public class QUIZIF210119083RAFSANZENMUSTAOFA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ QUIZ-IF2-10119083-RAFSANZENMUSTAOFA/src/quiz/if2/pkg10119083/rafsanzenmustaofa/ServiceItem.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz.if2.pkg10119083.rafsanzenmustaofa;
+
+/**
+ *
+ * Rafsan Zen
+ * 10119083
+ */
+public class ServiceItem {
+
+    public void displayService();
+    public float getPrice(int serviceItem);
+    public boolean checkMemberStatus(String statusMember);
+    public float getSale(boolean isMember, float parServicePrice);
+
+
+}
+
